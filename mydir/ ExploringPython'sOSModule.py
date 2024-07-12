8017b82711a254e4ff1129b7446a5d74a10e3237
codegirlMaya1 <ebonyslove209@gmail.com>
2024-07-11 22:57:18 -0600

    Add files via upload

---
+++ mydir/ ExploringPython'sOSModule.py
@@ -0,0 +1,11 @@
+import os
+
+old_path='hi_file.txt'
+new_path=os.path.join('subdir','hi_file.txt') 
+os.rename(old_path,new_path)
+print(" Please make a selection! Enter 1 to see old path or 2 to see this files new path")
+user=int(input(" Type 1 or 2: "))
+if user==1:
+    print(old_path) 
+elif  user==2:
+    print(new_path)   
